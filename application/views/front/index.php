214fbaa779a07c648fc209f92aee41e51efad8fb
Wallace Myem <wallacemyem@hotmail.com>
2021-08-06 15:02:10 +0100

    small fix on phone n css

--- application/views/front/index.php
+++ application/views/front/index.php
@@ -95,10 +95,10 @@
                   </h1>
                   <div class="mt-5">
                     <div class="left-50 btn-left1">
-                      <div class="wow-outer"><a class="button button-md button-primary-light button-winona wow slideInLeft" href="<?= base_url() ?>about" data-wow-delay="3.5s">Learn More</a></div>
+                      <div class="wow-outer"><a class="button button-md button-primary-light button-winona wow slideInLeft" href="<?= base_url() ?>about" data-wow-delay="1.5s">Learn More</a></div>
                     </div>
                     <div class="left-50">
-                      <div class="wow-outer"><a class="button button-md button-primary button-winona wow slideInRight" href="#" data-wow-delay="4.3s">Apply Now</a></div>
+                      <div class="wow-outer"><a class="button button-md button-primary button-winona wow slideInRight" href="#" data-wow-delay="1.3s">Apply Now</a></div>
                     </div>
                   </div>
                 </div>
@@ -194,7 +194,7 @@
       <section class="section section-lg text-center bg-default">
         <div class="container">
           <div class="row">
-            <div class="col-md-6 col-lg-4 col-xl-4 text-right pr-5 pb-5">
+            <div class="col-md-6 col-lg-4 col-xl-8 text-right pr-5 pb-5">
               <h3 class="section-title smaller-title wow-outer text-left"><span class="wow slideInUp">How to get started</span></h3>
               <br>
               <div class="number-wrap">
@@ -233,10 +233,10 @@
               </div>
             </div>
             <div class="col-lg-3 col-xl-3 text-left mobile-phone mb-3"></div>
-            <div class="col-md-6 col-lg-5 col-xl-5 text-left">
+            <!-- <div class="col-md-12 col-lg-8 col-xl-5 text-right">
             <h3 class="section-title smaller-title wow-outer text-left"><span class="wow slideInUp">What's Next?</span></h3>
-              <div class="number-wrap number-wrap-2 mt-4">
-                <div class="number-item " data-wow-delay="0.5s">
+              <div class="number-wrap number-wrap-2 mt-4 text-right">
+                <div class="number-item" data-wow-delay="0.5s">
                   <div class="number">
                     <svg class="Path_37842" viewBox="177.97 273.636 19.678 14.478"><path id="Path_37842" d="M 186.5176391601562 288.1143188476562 C 185.9148254394531 288.1143188476562 185.3332824707031 287.8836669921875 184.8937072753906 287.4674072265625 L 178.70849609375 281.6122436523438 C 177.7610626220703 280.7153930664062 177.7200927734375 279.22021484375 178.6169281005859 278.2727661132812 C 179.5137634277344 277.3253784179688 181.0087280273438 277.2841796875 181.9563903808594 278.1812133789062 L 186.3814849853516 282.3700561523438 L 193.5289001464844 274.41943359375 C 194.4010925292969 273.4491271972656 195.8946990966797 273.369873046875 196.8650207519531 274.2418518066406 C 197.8350830078125 275.1140441894531 197.9145660400391 276.607666015625 197.0423583984375 277.5779418945312 L 188.2743835449219 287.3312377929688 C 187.8484802246094 287.8048400878906 187.2494354248047 288.0865478515625 186.6128234863281 288.1123046875 C 186.5810241699219 288.1136474609375 186.5492248535156 288.1143188476562 186.5176391601562 288.1143188476562 Z M 186.5176391601562 288.1143188476562"></path></svg>
                   </div>
@@ -272,7 +272,7 @@
                   <div class="spacer" style="clear: both;"></div>
                 </div>
               </div>
-            </div>
+            </div> -->
             <div class="col-lg-12 col-xl-12 section-img text-center">
               <div class="wow-outer"><a class="button button-md button-primary button-winona wow slideInUp" href="<?= Utils::appUrl ?>signup" data-wow-delay="2.3s">Start Now</a></div>
             </div>
